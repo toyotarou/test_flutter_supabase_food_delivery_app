27f7c249bd6e3f07c9fc01faf3bda5e48b5144c5
toyodahideyuki <toyodahideyuki4819@gmail.com>
2025-04-29 19:53:32 +0900

    commit 20250429 login

--- lib/pages/auth/login_screen.dart
+++ lib/pages/auth/login_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../../service/auth_service.dart';
 import '../../widgets/my_button.dart';
+import '../../widgets/snack_bar.dart';
+import '../screen/onbording_screen.dart';
 import 'signup_screen.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -14,6 +17,12 @@ class _LoginScreenState extends State<LoginScreen> {
   TextEditingController emailEditingController = TextEditingController();
 
   TextEditingController passwordEditingController = TextEditingController();
+
+  final AuthService authService = AuthService();
+
+  bool isLoading = false;
+
+  bool isPasswordHidden = true;
 
   ///
   @override
@@ -45,7 +54,23 @@ class _LoginScreenState extends State<LoginScreen> {
 
             const SizedBox(height: 20),
 
-            MyButton(onTap: () {}, buttontext: 'Login'),
+            MyButton(
+              onTap: () {
+                _logIn();
+              },
+              buttontext: 'Login',
+            ),
+
+            const SizedBox(height: 30),
+
+            MyButton(
+              onTap: () {
+                emailEditingController.text = 'hide.toyoda@gmail.com';
+                passwordEditingController.text = 'hidechy4819';
+              },
+              buttontext: 'hide.toyoda@gmail.com',
+              color: Colors.orangeAccent,
+            ),
 
             const SizedBox(height: 30),
 
@@ -57,7 +82,11 @@ class _LoginScreenState extends State<LoginScreen> {
                 GestureDetector(
                   onTap: () {
                     // ignore: inference_failure_on_instance_creation, always_specify_types
-                    Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => const SignupScreen()));
+                    Navigator.pushReplacement(
+                      context,
+                      // ignore: inference_failure_on_instance_creation, always_specify_types
+                      MaterialPageRoute(builder: (BuildContext context) => const SignupScreen()),
+                    );
                   },
                   child: const Text(
                     'Sign Up Here',
@@ -71,5 +100,32 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
       ),
     );
+  }
+
+  ///
+  Future<void> _logIn() async {
+    final String email = emailEditingController.text.trim();
+
+    final String password = passwordEditingController.text.trim();
+
+    if (!mounted) {
+      return;
+    }
+
+    setState(() => isLoading = true);
+
+    final String? result = await authService.login(email, password);
+
+    if (result == null) {
+      setState(() => isLoading = false);
+
+      // ignore: inference_failure_on_instance_creation, use_build_context_synchronously, always_specify_types
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => const OnbordingScreen()));
+    } else {
+      setState(() => isLoading = false);
+
+      // ignore: use_build_context_synchronously
+      showSnackBar(context, 'Signup Failed:$result', Colors.red);
+    }
   }
 }
---
+++ lib/pages/screen/onbording_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OnbordingScreen extends StatefulWidget {
+  const OnbordingScreen({super.key});
+
+  @override
+  State<OnbordingScreen> createState() => _OnbordingScreenState();
+}
+
+class _OnbordingScreenState extends State<OnbordingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
